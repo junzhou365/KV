aa88e5013c511455dfc984cc8a24b12f48a2ef43
Julian Zhou <junzhou365@gmail.com>
2017-12-28 21:53:29 +0800

    Add the debug util file

---
+++ src/kvraft/util.go
@@ -0,0 +1,34 @@
+package raftkv
+
+import "log"
+import "os"
+
+// Debugging
+const Debug = 2
+
+func DPrintf(format string, a ...interface{}) (n int, err error) {
+	if Debug > 0 {
+		log.Printf(format, a...)
+	}
+	return
+}
+
+func DTPrintf(format string, a ...interface{}) (n int, err error) {
+	if Debug > 2 {
+		log.SetOutput(os.Stdout)
+		log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+		log.Printf(format, a...)
+	}
+	return
+}
+
+func DTESTPrintf(format string, a ...interface{}) (n int, err error) {
+	of := "\033[34m<<<<==== "
+	cf := " ====>>>>\033[0m\n"
+	if Debug > 1 {
+		log.SetOutput(os.Stdout)
+		log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+		log.Printf(of+format+cf, a...)
+	}
+	return
+}
